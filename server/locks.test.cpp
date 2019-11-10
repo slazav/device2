4b1f48afddb9f79098fa81f4a374573e06610eee
Vladislav Zavjalov <slazav@altlinux.org>
2019-11-10 19:52:13 +0200

    locks: add test

--- server/locks.test.cpp
+++ server/locks.test.cpp
@@ -11,6 +11,15 @@ int
 main(){
   try{
     Lock l1("a"), l2("a"), l3("b"), l4("b");
+
+    l1.lock();
+    l3.lock(); // l3 and l1 lock different names - no interaction
+
+    //l3.lock();  // this will block the program
+
+    l3.unlock();
+    l1.unlock();
+
   }
   catch (Err e) {
     std::cerr << "Error: " << e.str() << "\n";
