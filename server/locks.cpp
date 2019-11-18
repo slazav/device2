d9066c49254b5fd71261f72746e4a2a872a63394
Vladislav Zavjalov <slazav@altlinux.org>
2019-11-18 23:49:55 +0200

    locks.cpp: add global lock_for_locks

--- server/locks.cpp
+++ server/locks.cpp
@@ -6,6 +6,7 @@
 #define MHD_USE_POSIX_THREADS
 #include "mhd_locks.h"
 
+MHD_mutex_ lock_for_locks;
 
 std::map<std::string, std::shared_ptr<void> >
 Lock::mutexes;
@@ -14,7 +15,9 @@ Lock::Lock(const std::string & name): name(name){
   if (mutexes.count(name)==0){
     mutex.reset(new MHD_mutex_);
     MHD_mutex_init_((MHD_mutex_ *)mutex.get());
+    MHD_mutex_lock_(&lock_for_locks);
     mutexes.emplace(name, mutex);
+    MHD_mutex_unlock_(&lock_for_locks);
   }
   else {
     mutex = mutexes.find(name)->second;
@@ -24,7 +27,9 @@ Lock::Lock(const std::string & name): name(name){
 Lock::~Lock(){
   if (mutex.use_count() != 2) return;
   MHD_mutex_destroy_((MHD_mutex_ *)mutex.get());
+  MHD_mutex_lock_(&lock_for_locks);
   mutexes.erase(name);
+  MHD_mutex_unlock_(&lock_for_locks);
 }
 
 void
