619dd8536624017579cf5f320cf093568ef641f4
Vladislav Zavjalov <slazav@altlinux.org>
2020-09-29 18:02:08 +0100

    merge io_net, io_serial with drv_net, drv_serial

--- server/drv_net.cpp
+++ server/drv_net.cpp
@@ -1,33 +1,115 @@
 #include "drv_net.h"
 
-// read/write/open/close/fctl
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include <termios.h>
-
-// strerror
+#include <errno.h>
 #include <cstring>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
-Driver_net::Driver_net(const Opt & opts): opts(opts) {
-  net.reset(new IONet(opts));
+Driver_net::Driver_net(const Opt & opts) {
+// based on the example in
+// https://beej.us/guide/bgnet/html//index.html#a-simple-stream-client
+
+  int res;
+
+  //prefix for error messages
+  errpref = opts.get("errpref", "Driver_net: ");
+
+  // address and port (mandatory settings)
+  std::string addr = opts.get("addr", "");
+  if (addr == "") throw Err() << errpref
+    << "Parameter -addr is empty or missing";
+
+  std::string port = opts.get("port", "");
+  if (port == "") throw Err() << errpref
+    << "Parameter -port is empty or missing";
+
+  errpref += addr + ":" + port + ": ";
+
+  // fill hints structure and do getaddrinfo
+  struct addrinfo hints, *servinfo, *p;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  res = getaddrinfo(addr.c_str(), port.c_str(), &hints, &servinfo);
+  if (res != 0) throw Err() << errpref
+    << "getaddrinfo: " << gai_strerror(res);
+
+  // loop through all the results and connect to the first we can
+  int e;
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (sockfd == -1) {e = errno; continue; }
+    res = connect(sockfd, p->ai_addr, p->ai_addrlen);
+    if (res == -1) {e = errno; continue; }
+    break;
+  }
+  if (p == NULL) {
+    freeaddrinfo(servinfo);
+    throw Err() << errpref
+      << "can't connect: " << strerror(e);
+  }
+  freeaddrinfo(servinfo);
+
+  // set bufsize
+  bufsize = opts.get("bufsize", 4096);
+
+  // set timeout
+  timeout = opts.get("timeout", 5.0);
+
 }
 
 Driver_net::~Driver_net() {
-  net.reset();
+  ::close(sockfd);
+}
+
+std::string
+Driver_net::read() {
+  char buf[bufsize];
+
+  // Reading with timeout.
+  if (timeout > 0) {
+    // Prepare timeout structure and fd_set
+    struct timespec timeout_s;
+    timeout_s.tv_sec = int(timeout);
+    timeout_s.tv_nsec = (timeout - int(timeout))*1e9;
+    fd_set set;
+    FD_ZERO(&set); // clear the set
+    FD_SET(sockfd, &set);
+
+    // Wait for data.
+    int res = pselect(sockfd+1, &set, NULL, NULL, &timeout_s, NULL);
+    if (res == -1) throw Err() << errpref << "select error: " << strerror(errno);
+    if (res == 0)  throw Err() << errpref << "read timeout";
+  }
+
+  // Read data
+  int fl=0;
+  ssize_t ret = ::recv(sockfd, buf, sizeof(buf), fl);
+  if (ret<0) throw Err() << errpref << "read error: " << strerror(errno);
+  return std::string(buf, buf+ret);
+}
+
+void
+Driver_net::write(const std::string & msg) {
+  int fl = MSG_NOSIGNAL;
+  ssize_t ret = ::send(sockfd, msg.data(), msg.size(), fl);
+  if (ret<0) throw Err() << errpref << "write error: " << strerror(errno);
 }
 
 std::string
 Driver_net::ask(const std::string & msg) {
-  if (!net) throw Err() << "device is closed";
-  net->write(msg);
+  write(msg);
 
   // if we do not have '?' in the message
   // no answer is needed.
   if (msg.find('?') == std::string::npos)
     return std::string();
 
-  return net->read();;
+  return read();
 }
--- server/drv_net.h
+++ server/drv_net.h
@@ -4,19 +4,40 @@
 #include <memory>
 #include "drv.h"
 #include "opt/opt.h"
-#include "io_net.h"
 
 /*************************************************/
-// Network driver
+/*
+ * Access to a network device
+ *
+ * see https://beej.us/guide/bgnet/html//index.html
+
+Options:
+  -addr    -- Network address or IP.
+              Required.
+  -port    -- Port number.
+              Required.
+  -timeout -- Read timeout, seconds. No timeout if <=0.
+              Default 5.0.
+  -bufsize -- Buffer size for reading. Maximum length of read data.
+              Default: 4096
+  -errpref -- Prefix for error messages.
+              Default: "IOSerial: "
+*/
 
 class Driver_net: public Driver {
-  Opt opts;
-  std::shared_ptr<IONet> net;
+  int sockfd; // file descriptor for the network socket
+  size_t bufsize;
+  double timeout;
+  std::string errpref;
 
 public:
 
   Driver_net(const Opt & opts);
   ~Driver_net();
+
+  std::string read();
+  void write(const std::string & msg);
+
   std::string ask(const std::string & msg) override;
 };
 
