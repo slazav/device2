slazav/device2

commit ec896b90e34d668d8f8cdd007437192b21d984e9

drv_gpib, drv_net*, drv_serial*: use strings in trim, add, ack, nack options; change option names; read_words 1.4 is useful to read conf files with non-printable chars;

server/drv_gpib.cpp
@@ -56,7 +56,7 @@ Driver_gpib::Driver_gpib(const Opt & opts) {
 
   opts.check_unknown({"addr","board","timeout","open_timeout",
     "eot", "eos", "eos_mode", "secondary", "bufsize",
-    "errpref","idn", "add_ch", "trim_ch"});
+    "errpref","idn", "add_str", "trim_str"});
 
   //prefix for error messages
   errpref = opts.get("errpref", "gpib: ");
@@ -112,8 +112,8 @@ Driver_gpib::Driver_gpib(const Opt & opts) {
     }
 
     bufsize = opts.get("bufsize", 4096);
-    add     = opts.get("add_ch",  0xA);
-    trim    = opts.get("trim_ch", 0xA);
+    add     = opts.get("add_str",  "");
+    trim    = opts.get("trim_str", "");
     idn     = opts.get("idn", "");
   }
   catch (Err & e){
@@ -129,17 +129,24 @@ Driver_gpib::~Driver_gpib() {
 std::string
 Driver_gpib::read() {
   char buf[bufsize];
-  auto ret = ibrd(dh, buf, sizeof(buf));
+  auto res = ibrd(dh, buf, sizeof(buf));
   if (ibsta & ERR) throw Err() << errpref << error_text(iberr);
 
-  if (trim>0 && buf[ret-1]==trim) ret--;
-  return std::string(buf, buf+ret);
+  auto ret = std::string(buf, buf+res);
+
+  // -trim option
+  if (trim.size()>0 &&
+      ret.size() >= trim.size() &&
+      ret.substr(ret.size()-trim.size()-1) == trim){
+      ret.resize(ret.size()-trim.size());
+  }
+  return ret;
 }
 
 void
 Driver_gpib::write(const std::string & msg) {
   std::string m = msg;
-  if (add > 0) m+=(char)add;
+  if (add.size()>0) m+=add;
 
   auto ret = ibcmd(dh, m.data(), m.size());
   if (ibsta & ERR) throw Err() << errpref << error_text(iberr);

server/drv_net.cpp
@@ -15,7 +15,7 @@
 // https://beej.us/guide/bgnet/html//index.html#a-simple-stream-client
 Driver_net::Driver_net(const Opt & opts) {
   opts.check_unknown({"addr","port","timeout","bufsize","errpref","idn",
-    "add_ch", "trim_ch"});
+    "add_str", "trim_str"});
   int res;
 
   //prefix for error messages
@@ -57,8 +57,8 @@ Driver_net::Driver_net(const Opt & opts) {
 
   bufsize = opts.get("bufsize", 4096);
   timeout = opts.get("timeout", 5.0);
-  add     = opts.get("add_ch",  0xA);
-  trim    = opts.get("trim_ch", 0xA);
+  add     = opts.get("add_str",  "\n");
+  trim    = opts.get("trim_str", "\n");
   idn     = opts.get("idn", "");
 }
 
@@ -81,26 +81,33 @@ Driver_net::read() {
     FD_SET(sockfd, &set);
 
     // Wait for data.
-    int res = pselect(sockfd+1, &set, NULL, NULL, &timeout_s, NULL);
+    auto res = pselect(sockfd+1, &set, NULL, NULL, &timeout_s, NULL);
     if (res == -1) throw Err() << errpref << "select error: " << strerror(errno);
     if (res == 0)  throw Err() << errpref << "read timeout";
   }
 
   // Read data
   int fl=0;
-  ssize_t ret = ::recv(sockfd, buf, sizeof(buf), fl);
-  if (ret<0) throw Err() << errpref
+  auto res = ::recv(sockfd, buf, sizeof(buf), fl);
+  if (res<0) throw Err() << errpref
     << "read error: " << strerror(errno);
 
-  if (trim>0 && buf[ret-1]==trim) ret--;
-  return std::string(buf, buf+ret);
+  auto ret = std::string(buf, buf+res);
+
+  // -trim option
+  if (trim.size()>0 &&
+      ret.size() >= trim.size() &&
+      ret.substr(ret.size()-trim.size()-1) == trim){
+      ret.resize(ret.size()-trim.size());
+  }
+  return ret;
 }
 
 void
 Driver_net::write(const std::string & msg) {
 
   std::string m = msg;
-  if (add > 0) m+=(char)add;
+  if (add.size()>0) m+=add;
 
   int fl = MSG_NOSIGNAL;
   ssize_t ret = ::send(sockfd, m.data(), m.size(), fl);

server/drv_serial.cpp
@@ -78,7 +78,7 @@ Driver_serial::Driver_serial(const Opt & opts) {
     "echo","echoctl","echoe","echok","echoke","echonl","echoprt","extproc",
     "flusho","icanon","iexten","isig","noflsh","tostop","xcase", // local
     "parity","raw","sfc","nlcnv","lcase","timeout","vmin",
-    "delay","add_ch","trim_ch","ack_ch","nack_ch"});
+    "delay","add_str","trim_str","ack_str","nack_str"});
   int ret;
 
   //prefix for error messages
@@ -361,10 +361,10 @@ Driver_serial::Driver_serial(const Opt & opts) {
     << "can't set serial port parameters: " << strerror(errno);
 
   delay  = opts.get("delay",  0.1);
-  add    = opts.get("add_ch",  -1);
-  trim   = opts.get("trim_ch", -1);
-  ack    = opts.get("ack_ch",  -1);
-  nack   = opts.get("nack_ch", -1);
+  add    = opts.get("add_str");
+  trim   = opts.get("trim_str");
+  ack    = opts.get("ack_str");
+  nack   = opts.get("nack_str");
   idn    = opts.get("idn", "");
 }
 
@@ -376,32 +376,56 @@ Driver_serial::~Driver_serial() {
 
 std::string
 Driver_serial::read() {
-  char buf[4096]; // limit of the serial driver
-  ssize_t ret = ::read(fd,buf,sizeof(buf));
-  if (ret<0) throw Err() << errpref
-    << "read error: " << strerror(errno);
-  if (ret==0) throw Err() << errpref
-    << "read timeout";
 
-  // trim ack char
-  if (ack>0 && buf[ret-1]==ack) ret--;
+  std::string ret;
+  bool fail = false;
 
-  // trim nack char, return error
-  if (nack>0 && buf[ret-1]==nack) {
-    ret--;
-    throw Err() << errpref << "communication error";
+  while(1){
+    // read data, add to ret string
+    char buf[4096]; // limit of the serial driver
+    ssize_t res = ::read(fd,buf,sizeof(buf));
+    if (res<0) throw Err() << errpref
+      << "read error: " << strerror(errno);
+    if (res==0) throw Err() << errpref
+      << "read timeout";
+    ret += std::string(buf, buf+res);
+
+    // stop reading if -ack option is not set
+    if (ack.size()==0) break;
+
+    // if data ends with ack
+    if (ret.size() >= ack.size() &&
+        ret.substr(ret.size()-ack.size()-1) == ack){
+      ret.resize(ret.size()-ack.size());
+      break;
+    }
+
+    // if data ends with nack
+    if (nack.size()>0 &&
+        ret.size() >= nack.size() &&
+        ret.substr(ret.size()-nack.size()-1) == nack){
+      ret.resize(ret.size()-nack.size());
+      fail = true;
+      break;
+    }
+    // read more data if nack or ack are not found.
   }
 
-  // trim trm_ch (NL, CR)
-  if (trim>0 && buf[ret-1]==trim) ret--;
-  return std::string(buf, buf+ret);
+  // -trim option
+  if (trim.size()>0 &&
+      ret.size() >= trim.size() &&
+      ret.substr(ret.size()-trim.size()-1) == trim){
+      ret.resize(ret.size()-trim.size());
+  }
+  if (fail) throw Err() << "nack from the device: " << ret;
+  return ret;
 }
 
 void
 Driver_serial::write(const std::string & msg) {
 
   std::string m = msg;
-  if (add > 0) m+=(char)add;
+  if (add.size() > 0) m+=add;
 
   ssize_t ret = ::write(fd, m.data(), m.size());
   if (ret<0) throw Err() << errpref

server/drv_gpib.h
@@ -43,11 +43,11 @@ Options:
   -idn <str>     -- Override output of *idn? command.
                     Default: empty string, do not override.
 
-  -add_ch <N>    -- Add character to each message sent to the device.
-                    Default: '\n'
+  -add_str <v>   -- Add string to each message sent to the device.
+                    Default: "\n"
 
-  -trim_ch <N>   -- Remove character from the end of recieved messages.
-                    Default: '\n'
+  -trim_str <v>  -- Remove string from the end of recieved messages.
+                    Default: "\n"
 
 */
 
@@ -56,7 +56,7 @@ protected:
   int dh; // GPIB device handler
   size_t bufsize;
   std::string errpref,idn;
-  int add,trim;
+  std::string add,trim;
 
   // convert timeout
   int get_timeout(const std::string & s);

server/drv_net.h
@@ -25,10 +25,10 @@ Options:
                     Default: "IOSerial: "
   -idn <str>     -- Override output of *idn? command.
                     Default: empty string, do not override.
-  -add_ch <N>    -- Add character to each message sent to the device.
-                    Default: '\n'
-  -trim_ch <N>   -- Remove character from the end of recieved messages.
-                    Default: '\n'
+  -add_str <v>   -- Add string to each message sent to the device.
+                    Default: "\n"
+  -trim_str <v>  -- Remove string from the end of recieved messages.
+                    Default: "\n"
 */
 
 class Driver_net: public Driver {
@@ -37,7 +37,7 @@ protected:
   size_t bufsize;
   double timeout;
   std::string errpref,idn;
-  int add,trim;
+  std::string add,trim;
 
 public:
 

server/drv_net_gpib_prologix.h
@@ -56,7 +56,7 @@ public:
 
   // set options for net driver
   Opt set_opt(const Opt & opts){
-    // Options "add_ch", "trim_ch" are not needed, we want to use
+    // Options "add_str", "trim_str" are not needed, we want to use
     // default values, or set them here.
     opts.check_unknown({"addr","port","timeout","bufsize","errpref","idn"});
 

server/drv_serial.h
@@ -122,30 +122,29 @@ Options:
 
   Other settings:
 
-  -delay <val>   -- delay after write command, s.
+  -delay <v>     -- Delay after write command, s.
                     Default: 0.1
 
-  -errpref <str> -- Prefix for error messages.
+  -errpref <v>   -- Prefix for error messages.
                     Default: "serial: "
 
-  -idn <str>     -- Override output of *idn? command.
+  -idn <v>       -- Override output of *idn? command.
                     Default: empty string, do not override.
 
-  -add_ch <N>    -- Add character to each message sent to the device.
+  -add_str <v>   -- Add character to each message sent to the device.
                     Note: using terminal setting -onlcr you can convert NL to NL+CR
-                    Default: -1 (no adding)
+                    Default: empty string
 
-  -trim_ch <N>   -- remove character from the end of recieved messages.
-                    Default: -1 (no trimming)
+  -trim_str <v>  -- Remove character from the end of recieved messages.
+                    Default: empty string
 
-
-  -ack_ch <N>    -- Some devices send ack/nack chars at the end of message.
-                    This option set the ack char to trim it.
-                    Default: -1 (no ack char processing)
-
-  -nack_ch <N>   -- Some devices send ack/nack chars at the end of message.
-                    This option set the nack char to trim it and return an error.
-                    Default: -1 (no nack char processing)
+  -ack_str <v>
+  -nack_str <v>  -- Some devices send ack/nack sequences at the end of each message.
+                    It can be ack/nack characters (0x9/0x15), or "ok"/"#?" strings.
+                    if -ack option is set then reading is done until ack (or nack
+                    if it is set too) sequence is met. The sequences are removed
+                    from the output message; error is returned if nack recieved.
+                    Default: empty strings.
 
 Note that most options have no defaults: if such an option is not set
 then the setting is left untouched.
@@ -161,7 +160,7 @@ then the setting is left untouched.
 class Driver_serial: public Driver {
   int fd; // file descriptor
   std::string errpref, idn;
-  int ack,nack,add,trim;
+  std::string ack,nack,add,trim;
   double delay;
 
 public:

server/drv_serial_asm340.h
@@ -41,10 +41,10 @@ class Driver_serial_asm340: public Driver_serial {
     o.put("raw", 1);     // raw mode!
     o.put("delay", 0.1); // 100ms delay after write
     o.put("opost", 0);   // no output postprocessing
-    o.put("ack_ch", 0x06);  // trim ack char
-    o.put("nack_ch", 0x15); // trim nack char, return communication errors
-    o.put("add_ch",  0x0D); // add CR to each sent message
-    o.put("trim_ch", 0x0D); // trim CR from each recieved message
+    o.put("ack_str", "\x06"); // trim ack char
+    o.put("nack_str","\x15"); // trim nack char, return communication errors
+    o.put("add_str", "\r");   // add CR to each sent message
+    o.put("trim_str","\r");   // trim CR from each recieved message
     // set defaults (only it no values are set by user)
     o.put_missing("timeout", 5.0);
     o.put_missing("sfc", 1);

commit 0eb667e8e3c284e0da8a4cdf46ec2bbf1fee0cfe

fix error in message trimming introduced in ec896b90e34d668d8f8cdd007437192b21d984e9

server/drv_gpib.cpp
@@ -137,7 +137,7 @@ Driver_gpib::read() {
   // -trim option
   if (trim.size()>0 &&
       ret.size() >= trim.size() &&
-      ret.substr(ret.size()-trim.size()-1) == trim){
+      ret.substr(ret.size()-trim.size()) == trim){
       ret.resize(ret.size()-trim.size());
   }
   return ret;

server/drv_net.cpp
@@ -97,7 +97,7 @@ Driver_net::read() {
   // -trim option
   if (trim.size()>0 &&
       ret.size() >= trim.size() &&
-      ret.substr(ret.size()-trim.size()-1) == trim){
+      ret.substr(ret.size()-trim.size()) == trim){
       ret.resize(ret.size()-trim.size());
   }
   return ret;

server/drv_serial.cpp
@@ -395,7 +395,7 @@ Driver_serial::read() {
 
     // if data ends with ack
     if (ret.size() >= ack.size() &&
-        ret.substr(ret.size()-ack.size()-1) == ack){
+        ret.substr(ret.size()-ack.size()) == ack){
       ret.resize(ret.size()-ack.size());
       break;
     }
@@ -403,7 +403,7 @@ Driver_serial::read() {
     // if data ends with nack
     if (nack.size()>0 &&
         ret.size() >= nack.size() &&
-        ret.substr(ret.size()-nack.size()-1) == nack){
+        ret.substr(ret.size()-nack.size()) == nack){
       ret.resize(ret.size()-nack.size());
       fail = true;
       break;
@@ -414,7 +414,7 @@ Driver_serial::read() {
   // -trim option
   if (trim.size()>0 &&
       ret.size() >= trim.size() &&
-      ret.substr(ret.size()-trim.size()-1) == trim){
+      ret.substr(ret.size()-trim.size()) == trim){
       ret.resize(ret.size()-trim.size());
   }
   if (fail) throw Err() << "nack from the device: " << ret;